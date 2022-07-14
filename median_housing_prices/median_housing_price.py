0138e16d30101b0b27953064df45853d2a87aa0f
Karvendhan M <karvsmech@gmail.com>
2022-07-14 14:52:44 +0530

    crc32 for splitting training and testset

--- median_housing_prices/median_housing_price.py
+++ median_housing_prices/median_housing_price.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import tarfile
 from six.moves import urllib
+from zlib import crc32
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join('datasets', 'housing')
@@ -35,6 +36,16 @@ def split_train_test(data, test_ratio=0.2):
     return data.iloc[train_indices], data.iloc[test_indices]
 
 
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < (test_ratio * 2**32)
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
 if __name__ == '__main__':
     fetch_housing_data()
     housing = load_housing_data()
@@ -46,7 +57,10 @@ if __name__ == '__main__':
     housing.hist(bins=50, figsize=(20, 15))
     plt.show()
 
-    train_set, test_set = split_train_test(housing, 0.2)
+    # train_set, test_set = split_train_test(housing, 0.2)
+    housing_with_id = housing.reset_index()
+    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+    print('just for debugging')
 
 
 
