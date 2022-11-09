257f85402b6f6bd246705cda112bfbafb338912b
karvendhanm <karvsmech@gmail.com>
2022-11-09 13:47:14 +0530

    categorical variable and stratified shuffle split

--- median_housing_prices/load_housing_data.py
+++ median_housing_prices/load_housing_data.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
 
 from median_housing_prices import HOUSING_DATA_LOCAL_PATH
 
-def load_housing_data(housing_path = HOUSING_DATA_LOCAL_PATH):
+
+def load_housing_data(housing_path=HOUSING_DATA_LOCAL_PATH):
     '''
 
     :param housing_path:
@@ -12,6 +15,7 @@ def load_housing_data(housing_path = HOUSING_DATA_LOCAL_PATH):
     '''
     csv_path = os.path.join(housing_path, 'housing.csv')
     return pd.read_csv(csv_path)
+
 
 if __name__ == '__main__':
     df_housing = load_housing_data()
@@ -21,6 +25,20 @@ if __name__ == '__main__':
     df_housing.describe()
 
     # drawing histogram of all variable
-    df_housing.hist(bins=50, figsize=(20, 15))
-    plt.show()
+    # df_housing.hist(bins=50, figsize=(20, 15))
+    # plt.show()
 
+    # making a categorical variable out of the median income column
+    df_housing['income_cat'] = pd.cut(df_housing['median_income'],
+                                      bins=[0., 1.5, 3.0, 4.5, 6.0, np.inf],
+                                      labels=[1, 2, 3, 4, 5])
+
+    df_housing['income_cat'].hist()
+
+    # since median income has been converted to a categorical variable, we can use
+    # stratified sampling
+    split = StratifiedShuffleSplit()
+
+
+
+    print('this is just for debugging')
