30b2b81caf9e63e54e448c3cc4ba71c03f9c6f2d
karvendhanm <karvsmech@gmail.com>
2022-11-12 13:51:18 +0530

    common preprocessing techniques

--- median_housing_prices/data_preparation_for_ML_algos.py
+++ median_housing_prices/data_preparation_for_ML_algos.py
@@ -1,9 +1,14 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 import numpy as np
 import os
 import pandas as pd
 
-from sklearn.model_selection import StratifiedShuffleSplit
 from median_housing_prices import HOUSING_DATA_LOCAL_PATH
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 
 
 def load_housing_data(housing_path=HOUSING_DATA_LOCAL_PATH):
@@ -21,7 +26,6 @@ if __name__ == '__main__':
     is_plot = 0
 
     df_housing = load_housing_data()
-
     df_housing['income_cat'] = pd.cut(df_housing['median_income'],
                                       bins=[0., 1.5, 3.0, 4.5, 6.0, np.inf],
                                       labels=[1, 2, 3, 4, 5])
@@ -34,9 +38,49 @@ if __name__ == '__main__':
     for set_ in (strat_test_set, strat_train_set):
         set_.drop('income_cat', inplace=True, axis=1)
 
+    # putting the test set aside and focusing only on training dataset.
     housing = strat_train_set.copy()
     housing['rooms_per_household'] = housing[['total_rooms', 'households']].apply(lambda x: x[0] / x[1], axis=1)
     housing['bedrooms_per_room'] = housing[['total_bedrooms', 'total_rooms']].apply(lambda x: x[0] / x[1], axis=1)
     housing['population_per_household'] = housing[['population', 'households']].apply(lambda x: x[0] / x[1], axis=1)
 
     corr_matrix = housing.corr()
+
+    # prepare the data for machine learning algorithms.
+    # dropping the target variable
+    housing = strat_train_set.drop('median_house_value', axis=1)
+    housing_labels = strat_train_set['median_house_value'].copy()
+
+    # data cleaning
+    imputer = SimpleImputer(strategy='median')
+    # median can only be calculated on numerical attributes.
+    # creating a copy of the data without non-numerical attributes
+    housing_num = housing.drop('ocean_proximity', axis=1)
+    imputer.fit(housing_num)
+
+    housing_num.info()
+    X = imputer.transform(housing_num)
+    housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing_num.index)
+    housing_tr.info()
+
+    # handling text and categorical attributes
+    housing_cat = housing[['ocean_proximity']]
+    housing_cat.head(10)
+    housing_cat.value_counts()
+
+    # The drawback of ordinal encoder is ML algortihms will assume that two nearby values are more
+    # similar to each other than two distant values.
+    ordinal_encoder = OrdinalEncoder()
+    housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+    ordinal_encoder.categories_
+
+    cat_encoder = OneHotEncoder()
+    housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+    housing_cat_1hot
+    # csr matrix (compressed sparse row matrix) into normal numpy dense array
+    housing_cat_1hot.toarray()
+
+    cat_encoder.categories_
+
+    print('this is just for debugging')
+
