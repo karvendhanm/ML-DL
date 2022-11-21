0361cc051ea956426b3fa0816d839557d2822632
karvendhanm <karvsmech@gmail.com>
2022-11-22 04:27:49 +0530

    fixing a typo

--- median_housing_prices/data_preparation_for_ML_algos.py
+++ median_housing_prices/data_preparation_for_ML_algos.py
@@ -152,5 +152,10 @@ if __name__ == '__main__':
     lin_reg = LinearRegression()
     lin_reg.fit(housing_prepared, housing_labels)
 
-
+    # testing the model using some training data itself.
+    some_data = housing.iloc[:5, :]
+    some_labels = housing_labels.iloc[:5]
+    some_data_prepared = full_pipeline.transform(some_data)
+    print("Predictions:", lin_reg.predict(some_data_prepared))
+    print("Labels:", list(some_labels))
     print('this is just for debugging')
