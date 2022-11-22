88e25431e90f88655bc65f154644796ea9206e26
karvendhanm <karvsmech@gmail.com>
2022-11-22 06:55:45 +0530

    load housing data using pathlib and tarball

---
+++ median_housing_prices/ageron_end_to_end_machine_learning.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+import sklearn
+import sys
+import tarfile
+import urllib.request
+
+from packaging import version
+
+assert sys.version_info >= (3, 7)
+assert version.parse(sklearn.__version__) >= version.parse('1.0.1')
+
+
+def load_housing_data():
+    '''
+
+    :return:
+    '''
+    tarball_path = Path('datasets/housing.tgz')
+    if not tarball_path.is_file():
+        Path('datasets').mkdir(parents=True, exist_ok=True)
+        url = 'https://github.com/ageron/data/raw/main/housing.tgz'
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path='datasets')
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+
+if __name__ == '__main__':
+    housing = load_housing_data()
+
