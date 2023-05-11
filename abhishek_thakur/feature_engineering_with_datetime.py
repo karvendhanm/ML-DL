f791d07e6ae5773c69a7f7062c19a5fa7c761ef9
Karvendhan M <karvsmech@gmail.com>
2023-05-11 07:13:14 +0530

    feature engineering from a datetime column

--- abhishek_thakur/feature_engineering_with_datetime.py
+++ abhishek_thakur/feature_engineering_with_datetime.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-s = pd.date_range('2020-01-06', '2020-01-10', freq='10H').to_series()
+s = pd.date_range('2020-01-06', '2020-02-07', freq='7D').to_series()
 
 # create some features based on datetime
 features = {
@@ -11,3 +11,15 @@ features = {
     'quarter': s.dt.quarter.values,
     'weekofyear': s.dt.isocalendar().week.values
 }
+
+_dict = {
+    'date': list(s),  # a series of time stamps
+    'customer_id': [146361, 180838, 157857, 159772, 80014],
+    'cat1': [2, 4, 3, 5, 3],
+    'cat2': [2, 1, 3, 1, 2],
+    'cat3': [0, 0, 1, 1, 1],
+    'num1': [-0.518679, 0.415853, -2.061687, -0.276558, -1.456827]
+}
+
+df = pd.DataFrame.from_dict(_dict)
+print(df.dtypes)
