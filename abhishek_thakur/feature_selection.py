9312a65b933d2e2f551e954b683110938dbc20a3
Karvendhan M <karvsmech@gmail.com>
2023-05-24 04:18:43 +0530

    standardization & normalization and their effect on variance

--- abhishek_thakur/feature_selection.py
+++ abhishek_thakur/feature_selection.py
@@ -2,13 +2,27 @@ import numpy as np
 import pandas as pd
 
 from sklearn.feature_selection import VarianceThreshold
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 # create a random numpy array with 10 samples
 # and 6 features and values ranging between 1 and 15.
-X = np.random.randint(low=0, high=2, size=(10, 6))
+X = np.random.randint(low=0, high=12, size=(10, 6))
 df = pd.DataFrame(X, columns=[f"f_{i}" for i in range(1, 7)])
 
-df.f_1.var()
+for i in range(5):
+    print(df.iloc[:, i].var())
 
-var_thresh = VarianceThreshold(threshold=0.23)
+scl = StandardScaler()
+X_scl = scl.fit_transform(df)
+
+for i in range(5):
+    print(X_scl[:, i].var())
+
+minmax = MinMaxScaler()
+X_minmax = minmax.fit_transform(df)
+
+for i in range(5):
+    print(X_minmax[:, i].var())
+
+var_thresh = VarianceThreshold(threshold=4)
 transormed_data = var_thresh.fit_transform(df)
