fa3f1ddecf9d8313efc6e323661811eb74cc3b18
Karvendhan M <karvsmech@gmail.com>
2023-05-23 07:26:43 +0530

    log transformation of a variable

--- abhishek_thakur/creating_polynomial_features.py
+++ abhishek_thakur/creating_polynomial_features.py
@@ -18,3 +18,13 @@ df_transformed = pd.DataFrame(poly_feats, columns=[f"f_{i}" for i in range(1, nu
 # binning - converting numerical features to categorical
 df['f_bin_10'] = pd.cut(df['f_1'], bins=10, labels=False)
 df['f_bin_100'] = pd.cut(df['f_1'], bins=100, labels=False)
+
+# variance of the feature
+df.f_1.var()
+df.f_2.var()
+df.f_bin_10.var()
+df.f_bin_100.var()
+
+# log transformation of a variable
+df.f_1.apply(lambda x: np.log(1 + x)).var()
+
