3ec4c381eb592a78971c6a5d3bec592d867080f8
Karvendhan M <karvsmech@gmail.com>
2023-08-04 08:09:18 +0530

    missing data imputation

---
+++ feature_engineering/config.py
@@ -0,0 +1,6 @@
+directory = 'feature_engineering/'
+data_location = 'data/'
+foldername = 'credit_approval/'
+filename = 'crx.data'
+
+filepath = './' + directory + data_location + foldername + filename
